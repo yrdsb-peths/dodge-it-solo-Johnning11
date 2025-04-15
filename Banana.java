9c55d6d4dea46f3ce728656ba6586c273ee88d48
Johnning11 <350797049@gapps.yrdsb.ca>
2025-04-15 14:33:23 -0400

    move Ban

--- Banana.java
+++ Banana.java
@@ -7,5 +7,16 @@ public class Banana extends Actor
     public void act()
     {
         move(-4);
+        if(getX() <= 0) {
+            resetbanana();
+        }
+    }
+    public void resetbanana() {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num ==0){
+            setLocation(600, 100);
+        }else{
+            setLocation(600, 300);
+        }
     }
 }
